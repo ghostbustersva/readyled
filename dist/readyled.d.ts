98394a38b306a5adc09f48c502dc8aa511e1013b
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2026-03-19 10:32:03 -0400

    add declaration emit

---
+++ dist/readyled.d.ts
@@ -0,0 +1,12 @@
+export type ReadyLEDParams = {
+    fallbackFont?: string;
+    font?: string;
+    fontCheckInterval?: number;
+    maxWait?: number;
+    pixelHeight: number;
+    scrollSpeed?: number;
+    signWidth?: number;
+    target: HTMLElement;
+    text: string;
+};
+export declare const readyLED: (params: ReadyLEDParams) => Promise<void>;
